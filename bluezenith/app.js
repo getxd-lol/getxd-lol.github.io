c985d059f8b844db511dd974f39beee7f131879f
getxd <taminakarl18@gmail.com>
2022-05-02 14:32:59 +0800

    bluezenith website

---
+++ bluezenith/app.js
@@ -0,0 +1,8 @@
+tsParticles
+    .loadJSON("tsparticles", "presets/default.json")
+    .then((container) => {
+        console.log("callback - tsparticles config loaded");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
