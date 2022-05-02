93ccf7bad253313090de9fee2044f7f466e449fc
getxd <taminakarl18@gmail.com>
2022-05-02 14:33:43 +0800

    delete unused stuff

--- bluezenith/app.js
+++
@@ -1,8 +0,0 @@
-tsParticles
-    .loadJSON("tsparticles", "presets/default.json")
-    .then((container) => {
-        console.log("callback - tsparticles config loaded");
-    })
-    .catch((error) => {
-        console.error(error);
-    });
